a66974001ae1063a01832bb0c038911ed5cf42a0
Nitzan Ouzan <Nitzanouzan@mail.tau.ac.il>
2025-01-31 13:00:27 +0200

    fix formatting

--- instruction_memory.c
+++ instruction_memory.c
@@ -1,8 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include "instruction_memory.h"
-#include <string.h>
-#include <stdlib.h>
 
 uint32_t instruction_memory_read(instruction_memory_t *imem, uint32_t addr)
 {
@@ -12,15 +10,20 @@ uint32_t instruction_memory_read(instruction_memory_t *imem, uint32_t addr)
 void instruction_memory_load(instruction_memory_t *mem, FILE *imem)
 {
     char line[9]; // To store a line of 8 hex digits plus the null terminator
+    int i = 0;
+    uint32_t test;
 
     // Initialize the instructions array to 0
-    for (int i = 0; i < 1024; i++)
+    for (i = 0; i < 1024; i++)
     {
         mem->instructions[i] = 0;
     }
 
+    // Reset index for reading file
+    i = 0;
+
     // Read lines from the file and populate the instructions array
-    for (int i = 0; i < 1024 && fgets(line, sizeof(line), imem); i++)
+    while (i < 1024 && fgets(line, sizeof(line), imem))
     {
         // Strip any unwanted character
         line[strcspn(line, "\r\n")] = '\0';
@@ -31,9 +34,8 @@ void instruction_memory_load(instruction_memory_t *mem, FILE *imem)
             continue;
         }
         mem->instructions[i] = (uint32_t)strtol(line, NULL, 16);
+        i++;
     }
-
-    printf("Instructions memory:\n");
     for (int j = 0; j < 15; j++)
     {
         printf("% 08x\n", mem->instructions[j]);
